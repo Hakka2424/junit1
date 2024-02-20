45c46df6b386caf28d8d4608c6a3dc66d86137f4
Tyreke Hetzel <99774751+Hakka2424@users.noreply.github.com>
2024-02-20 01:38:41 -0700

    Create Circle2.java

---
+++ Circle2.java
@@ -0,0 +1,12 @@
+public class Circle2 extends Circle {
+
+   public Circle2(double x, double y, double radius) {
+       super(x, y, radius);
+   }
+
+   @Override
+   public boolean intersects(Circle other) {
+       double distance = Math.sqrt(Math.pow(center.x - other.center.x, 2) + Math.pow(center.y - other.center.y, 2));
+       return distance <= (this.radius + other.radius);
+   }
+}
