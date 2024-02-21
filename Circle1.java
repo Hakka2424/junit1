d7ae691cbef9a4436b5622aa7e75a8da97f6c468
Tyreke Hetzel <99774751+Hakka2424@users.noreply.github.com>
2024-02-20 17:18:22 -0700

    Update Circle1.java

--- Circle1.java
+++ Circle1.java
@@ -1,14 +1,24 @@
+/**
+ * A concrete implementation of a Circle that defines specific behavior for intersecting circles.
+ */
 public class Circle1 extends Circle {
 
-   public Circle1(double x, double y, double radius) {
-       super(x, y, radius);
-   }
+    /**
+     * Constructs a Circle1 instance with a specified center and radius.
+     *
+     * @param x The x-coordinate of the center.
+     * @param y The y-coordinate of the center.
+     * @param radius The radius of the circle.
+     */
+    public Circle1(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 
-   @Override
-   public boolean intersects(Circle other) {
-       // Calculate the distance between the centers of the two circles
-       double distance = Math.sqrt(Math.pow(center.x - other.center.x, 2) + Math.pow(center.y - other.center.y, 2));
-       // Two circles intersect if the distance between their centers is less than or equal to the sum of their radii
-       return distance <= (this.radius + other.radius);
-   }
+    @Override
+    public boolean intersects(Circle other) {
+        // Calculate the distance between the centers of the two circles
+        double distance = Math.sqrt(Math.pow(center.x - other.center.x, 2) + Math.pow(center.y - other.center.y, 2));
+        // Two circles intersect if the distance between their centers is less than or equal to the sum of their radii
+        return distance <= (this.radius + other.radius);
+    }
 }
