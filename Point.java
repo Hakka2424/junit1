9d0dd34037e7c5b7dbf50d1efe82a46f37c7b582
Tyreke Hetzel <99774751+Hakka2424@users.noreply.github.com>
2024-02-20 17:16:45 -0700

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,7 +1,22 @@
-
-public class Point
-{
+/**
+ * Represents a point in 2D space.
+ */
+public class Point {
+   /**
+    * The x-coordinate of this point.
+    */
    double x;
-   double y;
-}
 
+   /**
+    * The y-coordinate of this point.
+    */
+   double y;
+
+   /**
+    * Constructs a new point at the origin of the coordinate space (0, 0).
+    */
+   public Point() {
+       this.x = 0;
+       this.y = 0;
+   }
+}
